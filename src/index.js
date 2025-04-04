390cb9d548dfff915ef25740f0cdabed95468df0
Khush <kapvm4444@gmail.com>
2025-04-04 18:35:35 +0530

    Rating component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+import App from "./App";
+import RatingStar from "./components/RatingStar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test() {
+  const [rating, setRating] = useState(0);
+
+  return (
+    <>
+      <RatingStar onSetRate={setRating} maxRating={10} color={"Blue"} />
+      <p>The rating is {rating}</p>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Test />
+
+    <RatingStar
+      maxRating={5}
+      color={"red"}
+      size={64}
+      messages={["Worst", "Bad", "OK", "Good", "Wonderful"]}
+      defaultRating={0}
+      className={"test"}
+    />
+    {/*<App />*/}
+  </React.StrictMode>,
 );
